29343f40497c3ca0c79c88052b48ca3357de672c
constantin <constantinmaes@gmail.com>
2018-01-31 10:31:02 +0100

    Login view

---
+++ public_html/application/views/login_view.php
@@ -0,0 +1,7 @@
+
+
+<form method="post" action="">
+	<input type="text" name="login" placeholder="login" />
+	<input type="password" name="password" placeholder="password" />
+	<input type="submit" name="bLogin" value="Se connecter" />
+</form>
