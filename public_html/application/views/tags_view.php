7c7e743e793089d8a132d5be494ff95eafafe51e
constantin <constantinmaes@gmail.com>
2018-01-31 18:54:58 +0100

    Analyse par tags

--- public_html/application/views/tags_view.php
+++ public_html/application/views/tags_view.php
@@ -5,28 +5,12 @@
 <script>
 var ctx = document.getElementById("myChart");
 var myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'horizontalBar',
     data: {
         labels: [<?php echo $labels;?>],
         datasets: [{
             label: 'Fréquence des tags',
             data: [<?php echo $values;?>],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
             borderWidth: 1
         }]
     },
@@ -36,7 +20,12 @@ var myChart = new Chart(ctx, {
                 ticks: {
                     beginAtZero:true
                 }
-            }]
+            }],
+            xAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }],
         }
     }
 });
